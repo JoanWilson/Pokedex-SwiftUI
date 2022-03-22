e803f5466dbbd98b4dce55e224b49a76e2572142
Joan Wilson <joanwmoliveira@gmail.com>
2022-03-22 17:03:23 -0300

    first commit

---
+++ Pokedex/Api/Pokemon.swift
@@ -0,0 +1,8 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Joan Wilson Oliveira on 14/02/22.
+//
+
+import Foundation
---
+++ Pokedex/Views/PokemonImage.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonImage.swift
+//  Pokedex
+//
+//  Created by Joan Wilson Oliveira on 16/02/22.
+//
+
+import Foundation
